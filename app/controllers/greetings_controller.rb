8893fef748294714d4be43d81d893f043ef9c65e
Raghu Betina <rbetina@gmail.com>
2013-11-13 11:27:03 -0600

    Set up first challenge

--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -1,8 +1,17 @@
 class GreetingsController < ApplicationController
   def hello
+    @name = cookies[:name]
+    # In this action, utilize the favorite color cookie if it is set
   end
 
   def introduce
+    # In here, add a cookie to remember the person's favorite color also
+    the_name = params[:person]
+    cookies[:name] = the_name
     redirect_to '/hi'
+  end
+
+  def forget
+    # Create a link that will trigger this action and forget all info about the user
   end
 end
